06896bbc5caf142e901f5343d55141b42e033aff
snhrknsk <snhrknsk+git@gmail.com>
2020-07-20 01:48:55 +0900

    run chapter 2-6 sample

---
+++ ch2_6/func.js
@@ -0,0 +1,6 @@
+exports.add = function(val1,val2){
+    return val1 + val2;
+}
+exports.sub = function(val1,val2){
+    return val1 - val2;
+}
---
+++ ch2_6/sample.js
@@ -0,0 +1,7 @@
+/*
+例Create HTML using module
+node sample.js
+*/
+var func = require('./func');
+console.log(func.add(1,2));
+console.log(func.sub(10,3));
