315850240146d695a77da595bc71921290eeeab7
snhrknsk <snhrknsk+git@gmail.com>
2020-07-19 20:34:59 +0900

    run chapter 2_4 sample

---
+++ ch2_4/sample.js
@@ -0,0 +1,38 @@
+/*
+例Create HTML submit input data
+node sample.js
+http://localhost:1234
+*/
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+var indexPage = fs.readFileSync('./index.html', 'utf-8');
+
+var server = http.createServer(function(req, res){
+    if(req.method == 'GET'){
+        var urlParts = url.parse(req.url, true);
+        console.log('---GET Request---');
+        console.log('nameは'+urlParts.query.name);
+        console.log('ageは'+urlParts.query.age);
+    } else {
+        console.log('---POST Request---');
+        var body = "";
+        req.on('data', function(data){
+            body += data;
+        });
+        req.on('end', function(){
+            var params = qs.parse(body);            
+            console.log('nameは'+params.name);
+            console.log('ageは'+params.age);
+        })
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(indexPage);
+    res.end();
+});
+
+server.listen(1234);
+console.log('Start Server');
